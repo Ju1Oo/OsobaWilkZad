dec095af9fa46d6559d85494dfcd40c2c059c066
Ju1Oo <julianwidlak@gmail.com>
2025-04-09 17:06:21 +0200

    Osoba

---
+++ src/DataUrodzenia.java
@@ -0,0 +1,2 @@
+public class DataUrodzenia {
+}
---
+++ src/Osoba.java
@@ -0,0 +1,55 @@
+
+    public class Osoba {
+        private int id;
+        private String imie;
+        private String nazwisko;
+        private String nazwiskoPanienskie;
+        private Adres adres;
+        private DataUrodzenia dataUrodzenia;
+        private Osoba matka;
+        private Osoba ojciec;
+
+        Osoba(  String in_imie, String in_nazwisko, String in_nazwiskoPanienskie, Adres in_adres, DataUrodzenia in_dataUrodzenia, Osoba in_matka, Osoba in_ojciec)
+        {
+            imie = in_imie;
+            nazwisko = in_nazwisko;
+            nazwiskoPanienskie = in_nazwiskoPanienskie;
+            adres = in_adres;
+            dataUrodzenia = in_dataUrodzenia;
+            matka = in_matka;
+            ojciec = in_ojciec;
+        }
+
+        Osoba(  String in_imie, String in_nazwisko, String in_nazwiskoPanienskie, Adres in_adres, DataUrodzenia in_dataUrodzenia)
+        {
+            imie = in_imie;
+            nazwisko = in_nazwisko;
+            nazwiskoPanienskie = in_nazwiskoPanienskie;
+            adres = in_adres;
+            dataUrodzenia = in_dataUrodzenia;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) return true;
+            if (!(o instanceof Osoba osoba)) return true;
+            return (id == osoba.getId());
+        }
+        public String GetImie(){
+            return imie;
+        }
+        public String getNazwisko() {
+            return nazwisko;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public DataUrodzenia getDataUrodzenia() {
+            return dataUrodzenia;
+
+        }
+
+}
