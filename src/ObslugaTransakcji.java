e3c7fd2e46dd157b1b315bf312207fa39bdeb768
Ju1Oo <julianwidlak@gmail.com>
2025-04-23 17:07:56 +0200

    ObslugaTransakcji.java

---
+++ src/ObslugaTransakcji.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ObslugaTransakcji {
+    Scanner scanner = new Scanner(System.in);
+    public void addOsoba()
+    {
+        System.out.println("Podaj imie: ");         //dokoncz
+
+    }
+}
